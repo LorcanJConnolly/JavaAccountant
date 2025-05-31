0cbfc9532e271cc6bfaef2724fb01cc44437b71b
LorcanJConnolly <lconnolly.lorcan@gmail.com>
2025-05-31 17:24:07 +0100

    correct typing for AccountsManager changes

--- src/main/java/com/example/Client.java
+++ src/main/java/com/example/Client.java
@@ -3,7 +3,7 @@ import java.util.HashSet;
 import java.util.NoSuchElementException;
 
 public class Client {
-    public HashSet<JobManager> jobs;
+    public HashSet<AccountsManager> jobs;
     public final String name;
 
     public Client(String name){
@@ -11,29 +11,26 @@ public class Client {
         this.jobs = new HashSet<>();
     }
 
-    public Client(String name, HashSet<JobManager> jobs){
+    public Client(String name, HashSet<AccountsManager> jobs){
         this.name = name;
         this.jobs = jobs;
     }
 
-    public HashSet<JobManager> getJobs(){
+    public HashSet<AccountsManager> getJobs(){
         return this.jobs;
     }
 
-    public JobManager getJob(JobManager job){
-        for (JobManager job_manager : this.jobs){
+    public AccountsManager getJob(AccountsManager job){
+        for (AccountsManager job_manager : this.jobs){
             if (job == job_manager){return job_manager;}
         }
         return null;
     }
 
-    public void deleteJob(JobManager job){
-        for (JobManager job_manager : this.jobs){
+    public void deleteJob(AccountsManager job){
+        for (AccountsManager job_manager : this.jobs){
             if (job == job_manager){this.jobs.remove(job);}
         }
         throw new NoSuchElementException("Did not find job to delete.");
     }
 }
-
-// TODO: adjust data type of jobs for easy access by object reference
-// TODO: update tests for quick changes
