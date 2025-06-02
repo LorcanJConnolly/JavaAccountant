d6620883bf198ccbe7a5254e15f0dfe264c08ea5
LorcanJConnolly <lconnolly.lorcan@gmail.com>
2025-06-02 07:49:15 +0100

    added method placeholders

--- src/main/java/com/example/Account.java
+++ src/main/java/com/example/Account.java
@@ -1,4 +1,20 @@
 package com.example;
 
+import java.util.UUID;
+
 public class Account {
+    private final UUID id;
+    private int current_value;
+
+    public Account(){
+        this.id = UUID.randomUUID(); // generate a UUID for the Object
+    }
+
+    public UUID getId(){
+        return this.id;
+    }
+
+    public void applyAdjustment(){}
+
+    public void getValueAt(){}
 }
