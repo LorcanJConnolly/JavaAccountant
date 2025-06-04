b0bc622579f8bab6c287680d73afce39636bd7b0
LorcanJConnolly <lconnolly.lorcan@gmail.com>
2025-06-04 18:34:56 +0100

    First tests

--- src/test/java/com/example/AccountTest.java
+++ src/test/java/com/example/AccountTest.java
@@ -1,4 +1,41 @@
 package com.example;
 
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.Month;
+import java.util.ArrayList;
+
 public class AccountTest {
+    public void testGetValueAt(){
+        Account account = new Account();
+        Adjustment adjustment1 = new Adjustment("adjustment 1", 100);
+        Adjustment adjustment2 = new Adjustment("adjustment 2", -200);
+        Adjustment adjustment3 = new Adjustment("adjustment 3", -300);
+
+        LocalDateTime date1 = LocalDate.of(2025, Month.DECEMBER, 25).atStartOfDay();
+        LocalDateTime date2 = LocalDate.of(1999,Month.JANUARY, 31).atStartOfDay();
+        LocalDateTime date3 = LocalDate.of(2021, Month.MARCH, 26).atStartOfDay();
+
+        // Create Journals
+        Journal journal1 = new Journal("journal 1", adjustment1, date1);
+
+        ArrayList<Adjustment> adjustments2 = new ArrayList<>();
+        adjustments2.add(adjustment1);
+        adjustments2.add(adjustment2);
+        Journal journal2 = new Journal("journal 2", adjustments2, date2);
+
+        ArrayList<Adjustment> adjustments3 = new ArrayList<>();
+        adjustments3.add(adjustment1);
+        adjustments3.add(adjustment2);
+        adjustments3.add(adjustment3);
+        Journal journal3 = new Journal("journal 3", adjustments3, date3);
+
+        // Create Account
+        Account account = new Account("account", 1000, "___", )
+
+        // create account
+        // add journals
+
+        // retrieve account value at dates
+    }
 }
