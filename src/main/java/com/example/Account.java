0426631cdca760eb3b09ca930dff64411ae184c2
LorcanJConnolly <lconnolly.lorcan@gmail.com>
2025-06-07 15:59:22 +0100

    refactoring of attribute names and temporarily ignoring tests

--- src/main/java/com/example/Account.java
+++ src/main/java/com/example/Account.java
@@ -1,6 +1,5 @@
 package com.example;
 
-import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.util.*;
 
@@ -8,26 +7,26 @@ public class Account {
     private final UUID accountId;
     private final ArrayList<String> descriptors;
     private final double initialValue;
-    private AccountCategory category;
+    private String category;
     private HashMap<LocalDateTime, ArrayList<Journal>> history;
-    private ChartOfAccounts.Node COA_Index;
+    private ChartOfAccountsTree.Node COA_Index;
 
-    public Account(ArrayList<String> descriptors, double initialValue, ChartOfAccounts.Node COA_Index){
+    public Account(ArrayList<String> descriptors, double initialValue, ChartOfAccountsTree.Node COA_Index){
         this.accountId = UUID.randomUUID(); // generate a UUID for the Object
         this.descriptors = descriptors;
         this.initialValue = initialValue;
         this.history = new HashMap<LocalDateTime, ArrayList<Journal>>();
         this.COA_Index = COA_Index;
-        this.category = COA_Index.getRoot();
+        this.category = COA_Index.getChartOfAccounts().getRoot().getCategory();
     }
 
-    public Account(String descriptor, double initialValue, ChartOfAccounts.Node COA_Index){
+    public Account(String descriptor, double initialValue, ChartOfAccountsTree.Node COA_Index){
         this.accountId = UUID.randomUUID(); // generate a UUID for the Object
         this.descriptors = new ArrayList<>(Collections.singleton(descriptor));
         this.initialValue = initialValue;
         this.history = new HashMap<LocalDateTime, ArrayList<Journal>>();
         this.COA_Index = COA_Index;
-        this.category = COA_Index.getRoot();
+        this.category = COA_Index.getChartOfAccounts().getRoot().getCategory();
     }
 
     public UUID getAccountId(){
