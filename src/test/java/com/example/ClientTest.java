47f81d7004ae8871068308cf9f7151f41071074b
LorcanJConnolly <lconnolly.lorcan@gmail.com>
2025-06-09 17:29:01 +0100

    Updated for JUnit 5 and client class refactor

--- src/test/java/com/example/ClientTest.java
+++ src/test/java/com/example/ClientTest.java
@@ -4,74 +4,70 @@ import java.time.LocalDate;
 import java.time.Period;
 import java.util.HashSet;
 
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
 import com.example.domain.accountmanager.AccountsManager;
 import com.example.domain.client.Client;
-import junit.framework.TestCase;
 
-public class ClientTest extends TestCase {
+public class ClientTest {
+
+    @Test
     public void testInitialisation1(){
         Client test_client = new Client("test_client");
         assertEquals("test_client", test_client.name);
         assertTrue(test_client.getAccountManagerIds().isEmpty());
     }
 
+    @Test
     public void testInitialisation2(){
-        HashSet<AccountsManager> jobs = new HashSet<>();
-        AccountsManager job1 = new AccountsManager(LocalDate.now(), Period.ofMonths(6), "Test Job 1");
-        AccountsManager job2 = new AccountsManager(LocalDate.now(), Period.ofMonths(6), "Test Job 2");
-        jobs.add(job1);
-        jobs.add(job2);
-        Client client = new Client("test_client", jobs);
+        HashSet<String> accountsManagerIds = new HashSet<>();
+        accountsManagerIds.add("id1");
+        accountsManagerIds.add("id2");
+        Client client = new Client("test_client", accountsManagerIds);
         assertEquals("test_client", client.name);
-        assertEquals(jobs, client.getAccountManagerIds());
-    }
-
-    public void testCreateJob(){
-        Client client = new Client("test_client");
-        AccountsManager job1 = new AccountsManager(LocalDate.now(), "Test Job 1");
-        client.addAccountManagerId(job1);
         assertFalse(client.getAccountManagerIds().isEmpty());
     }
 
-    public void testCreateTheSameJob(){
-        // FIXME
+    @Test
+    public void testAddAccountManagerId(){
+
+        // Success
         Client client = new Client("test_client");
-        AccountsManager job1 = new AccountsManager(LocalDate.now(), "Test Job 1");
-        client.addAccountManagerId(job1);
-        // Junit 3.8.1 version of assertThrows
-        try {
-            client.addAccountManagerId(job1);
-            fail("Expected IllegalArgumentException");
-        } catch (IllegalArgumentException e) {
-            assertEquals("Job already exists in Client. Change attribute(s) of Job.", e.getMessage());
-        }
+        client.addAccountManagerId("id");
+        assertFalse(client.getAccountManagerIds().isEmpty());
 
+        // Failure
+        IllegalArgumentException exception = assertThrows(IllegalArgumentException.class, () -> {
+            client.addAccountManagerId("id");
+        });
     }
 
-    public void testGetJobs(){
-        HashSet<AccountsManager> jobs = new HashSet<>();
-        AccountsManager job1 = new AccountsManager(LocalDate.now(), Period.ofMonths(6), "Test Job 1");
-        AccountsManager job2 = new AccountsManager(LocalDate.now(), Period.ofMonths(6), "Test Job 2");
-        jobs.add(job1);
-        jobs.add(job2);
-        Client client = new Client("test_client", jobs);
+    @Test
+    public void testGetAccountManagerIds(){
+        HashSet<String> accountsManagerIds = new HashSet<>();
+        accountsManagerIds.add("id1");
+        accountsManagerIds.add("id2");
+        Client client = new Client("test_client", accountsManagerIds);
         assertEquals("test_client", client.name);
-        assertEquals(jobs, client.getAccountManagerIds());
+        // See testInitialisation2
+        assertEquals(accountsManagerIds, client.getAccountManagerIds());
     }
 
-    public void testDeleteJob(){
-        // TODO: possibly change "job" to AccountManager in some places
+
+    @Test
+    public void testRemoveAccountManagerId(){
+
         HashSet<AccountsManager> jobs = new HashSet<>();
-        AccountsManager job1 = new AccountsManager(LocalDate.now(), Period.ofMonths(6), "Test Job 1");
-        AccountsManager job2 = new AccountsManager(LocalDate.now(), Period.ofMonths(6), "Test Job 2");
-        jobs.add(job1);
-        jobs.add(job2);
-        Client client = new Client("test_client", jobs);
+        HashSet<String> accountsManagerIds = new HashSet<>();
+        accountsManagerIds.add("id1");
+        accountsManagerIds.add("id2");
+        Client client = new Client("test_client", accountsManagerIds);
         assertEquals("test_client", client.name);
-        assertEquals(jobs, client.getAccountManagerIds());
-        client.deleteJob(job1);
-        jobs.remove(job1);
-        assertEquals(jobs, client.getAccountManagerIds());
+        assertEquals(accountsManagerIds, client.getAccountManagerIds());
+        client.removeAccountManagerIds("id1");
+        assertEquals(1, client.getAccountManagerIds().size());
+        assertTrue(client.getAccountManagerIds().contains("id2"));
     }
 
 }
