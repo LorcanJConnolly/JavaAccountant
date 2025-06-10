b02fe9fed6d3f6d107b77e604758e7217e6bf12c
LorcanJConnolly <lconnolly.lorcan@gmail.com>
2025-06-10 21:13:37 +0100

    added adjustment into journal class for clarity

--- src/main/java/com/example/domain/model/journal/Journal.java
+++ src/main/java/com/example/domain/model/journal/Journal.java
@@ -3,13 +3,37 @@ package com.example.domain.model.journal;
 import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.NoSuchElementException;
 import java.util.UUID;
 
 public class Journal {
     private final UUID journalID;
     private final String description;
     private final ArrayList<Adjustment> adjustments;
+    private int total;
     private final LocalDateTime date;
+
+    public class Adjustment {
+        private final String description;
+        private final int value;
+
+        public Adjustment(String description, int value){
+            this.description = description;
+            this.value = value;
+        }
+
+        public String getDescription(){
+            return this.description;
+        }
+
+        public int getValue(){
+            return this.value;
+        }
+        @Override
+        public String toString(){
+            return "Adjustment(" + this.description + ", " + this.value + ")";
+        }
+    }
 
     public Journal(String description, ArrayList<Adjustment> adjustments, LocalDateTime date){
         this.journalID = UUID.randomUUID();
@@ -23,5 +47,18 @@ public class Journal {
         this.description = description;
         this.adjustments = new ArrayList<>(Collections.singletonList(adjustment));
         this.date = date;
+    }
+
+    public void addAdjustment(Adjustment adjustment){
+        total += adjustment.getValue();
+        adjustments.add(adjustment);
+    }
+
+    public void removeAdjustment(Adjustment adjustment){
+        if (!this.adjustments.contains(adjustment)){
+            throw new NoSuchElementException("Adjustment not found in journal: '" + adjustment + "'.");
+        }
+        total -= adjustment.getValue();
+        adjustments.remove(adjustment);
     }
 }
