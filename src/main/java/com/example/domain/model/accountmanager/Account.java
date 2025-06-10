b414b7e41f3d8f309fe4da3787398152367286e8
LorcanJConnolly <lconnolly.lorcan@gmail.com>
2025-06-10 21:12:02 +0100

    Refactor after finally understanding aggregates

--- src/main/java/com/example/domain/model/accountmanager/Account.java
+++ src/main/java/com/example/domain/model/accountmanager/Account.java
@@ -1,48 +1,48 @@
 package com.example.domain.model.accountmanager;
 
-import com.example.domain.model.journal.Adjustment;
 import com.example.domain.model.journal.Journal;
 
 import java.time.LocalDateTime;
 import java.util.*;
 
-//public class Account {
-//    private final UUID accountId;
-//    private final ArrayList<String> descriptors;
-//    private final double initialValue;
-//    private String category;
-//    private HashMap<LocalDateTime, HashMap<Integer, ArrayList<Journal>>> history;
-//    private ChartOfAccountsTree.Node COA_Index;
-//
-//    public Account(ArrayList<String> descriptors, double initialValue, ChartOfAccountsTree.Node COA_Index){
-//        this.accountId = UUID.randomUUID(); // generate a UUID for the Object
-//        this.descriptors = descriptors;
-//        this.initialValue = initialValue;
-//        this.history = new HashMap<LocalDateTime, ArrayList<Journal>>();
-//        this.COA_Index = COA_Index;
-//        this.category = COA_Index.getChartOfAccounts().getRoot().getCategory();
-//    }
-//
-//    public Account(String descriptor, double initialValue, ChartOfAccountsTree.Node COA_Index){
-//        this.accountId = UUID.randomUUID(); // generate a UUID for the Object
-//        this.descriptors = new ArrayList<>(Collections.singleton(descriptor));
-//        this.initialValue = initialValue;
-//        this.history = new HashMap<LocalDateTime, ArrayList<Journal>>();
-//        this.COA_Index = COA_Index;
-//        this.category = COA_Index.getChartOfAccounts().getRoot().getCategory();
-//    }
-//
-//    public UUID getAccountId(){
-//        return this.accountId;
-//    }
-//
-//    public void applyAdjustment(LocalDateTime date, Adjustment adjustment){
-//        int current_value = this.getValueAt(LocalDateTime.now());
-//        HashMap<int, Adjustment>
-//        history.put(date)
-//    }
-//
-//    public int getValueAt(LocalDateTime date){}
-//
-//    public ArrayList<Journal> getJournalsAt(LocalDateTime date){}
-//}
+public class Account {
+    private final UUID accountId;
+    private final ArrayList<String> descriptors;
+    private final double initialValue;
+    private AccountCategory category;
+    private AccountHistory history; // HashMap<LocalDateTime, HashMap<Integer, ArrayList<Journal>>>
+    private String chartOfAccountsNodeCategory;
+
+    public Account(ArrayList<String> descriptors, double initialValue, AccountCategory category, String chartOfAccountsNodeCategory) {
+        this.accountId = UUID.randomUUID(); // generate a UUID for the Object
+        this.descriptors = descriptors;
+        this.initialValue = initialValue;
+        this.category = category;
+        this.chartOfAccountsNodeCategory = chartOfAccountsNodeCategory;
+        this.history = AccountHistory(initialValue);
+    }
+
+    public UUID getAccountId(){
+        return this.accountId;
+    }
+
+    public ArrayList<String> getDescriptors(){
+        return new ArrayList<>(descriptors);
+    }
+
+    public double getInitialValue(){
+        return this.initialValue;
+    }
+
+    public AccountCategory getCategory(){
+        return this.category;
+    }
+
+    public String getChartOfAccountsNodeCategory(){
+        return this.chartOfAccountsNodeCategory;
+    }
+
+    public AccountHistory getHistory(){
+        return this.history;
+    }
+}
--- src/main/java/com/example/domain/model/accountmanager/AccountsManager.java
+++ src/main/java/com/example/domain/model/accountmanager/AccountsManager.java
@@ -3,41 +3,34 @@ package com.example.domain.model.accountmanager;
 import java.time.Period;
 import java.time.LocalDate;
 import java.util.HashMap;
+import java.util.NoSuchElementException;
 import java.util.Objects;
 import java.util.UUID;
 
 public class AccountsManager {
     private final UUID accountsManagerId;
     private String description;
-    private Period period_length;
-    private LocalDate period_start;
-    private final HashMap<UUID, Account> accounts;
     private final LocalDate initialisation_date;
+    private LocalDate period_start;
+    private Period period_length;
+    private final HashMap<UUID, Account> accounts;
+    private ChartOfAccountsTree chartOfAccounts;
 
-    public AccountsManager(LocalDate period_start, Period period_length, String description){
+    public AccountsManager(String description, LocalDate period_start, Period period_length, ChartOfAccountsTree chartOfAccounts){
         this.accountsManagerId = UUID.randomUUID(); // generate a UUID for the Object
+        this.description = description;
+        this.initialisation_date = LocalDate.now();
+        this.period_start = period_start;
         this.period_length = period_length;
-        this.period_start = period_start;
-        // TODO currency and reorder
-        this.description = description;
-        this.initialisation_date = LocalDate.now();
         this.accounts = new HashMap<>();
-    }
+        this.chartOfAccounts = chartOfAccounts;
 
-    public AccountsManager(LocalDate period_start, String description){
-        this.accountsManagerId = UUID.randomUUID(); // generate a UUID for the Object
-        this.period_length = Period.ofMonths(12);
-        this.period_start = period_start;
-        this.description = description;
-        this.initialisation_date = LocalDate.now();
-        this.accounts = new HashMap<>();
     }
 
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
-        if (!(o instanceof AccountsManager)) return false;
-        AccountsManager accounts_manager = (AccountsManager) o;
+        if (!(o instanceof AccountsManager accounts_manager)) return false;
         return this.getId() == accounts_manager.getId() &&
                 this.getPeriodLength() == accounts_manager.getPeriodLength() &&
                 this.getPeriodStart() == accounts_manager.getPeriodStart() &&
@@ -50,20 +43,28 @@ public class AccountsManager {
         return this.accountsManagerId;
     }
 
-    public Period getPeriodLength() {
-        return this.period_length;
+    public String getDescription(){
+        return this.description;
+    }
+
+    public LocalDate getInitialisationDate(){
+        return this.initialisation_date;
     }
 
     public LocalDate getPeriodStart(){
         return this.period_start;
     }
 
-    public String getDescription(){
-        return this.description;
+    public Period getPeriodLength() {
+        return this.period_length;
     }
 
     public HashMap<UUID, Account> getAccounts(){
         return new HashMap<>(this.accounts);
+    }
+
+    public ChartOfAccountsTree getChartOfAccounts(){
+        return this.chartOfAccounts;
     }
 
     public void changePeriodStart(LocalDate new_date){
@@ -74,25 +75,39 @@ public class AccountsManager {
         this.period_length = new_period_length;
     }
 
-    public void changeDescription(String new_description){
-        this.description = new_description;
+    public void changeDescription(String newDescription){
+        this.description = newDescription;
     }
 
-    public void addAccount(Account account){
-        if (this.accounts.containsKey(account.getAccountId())) {
+    public void changeChartOfAccounts(ChartOfAccountsTree newChartOfAccounts){
+        this.chartOfAccounts = newChartOfAccounts;
+    }
+
+    public void addAccount(UUID accountId){
+        Account account = findAccountById(accountId);
+
+        if (account != null){
             throw new IllegalArgumentException("Account already exists in AccountsManager.");
         }
-        this.accounts.put(account.getAccountId(), account);
-    }
 
-    public void deleteAccount(Account account){
-        if (!this.accounts.containsKey(account.getAccountId())) {
-            throw new IllegalArgumentException("Account does not exist in AccountsManager.");
+        if (!this.chartOfAccounts.showNodes().contains(Account.getChartOfAccountsIndex())){
+            throw new IllegalArgumentException("Account has no index in chart of accounts.");
         }
-        this.accounts.remove(account.getAccountId(), account);
+
+        this.accounts.put(accountId, account);
     }
 
-    public void adjustAccount(){}
+    public void deleteAccount(UUID accountId){
+        Account account = findAccountById(accountId);
 
+        if (account == null) {
+            throw new NoSuchElementException("Cannot find account with id '" + accountId + "'.");
+        }
 
+        // TODO: check this is correct annotation
+        this.accounts.remove(accountId, account);
+    }
+    public Account findAccountById(UUID accountId){
+        return this.accounts.get(accountId);
+    }
 }
