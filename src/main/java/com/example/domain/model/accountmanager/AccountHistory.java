64810f84fc5cd49863cb8021d9a3455a1fd38623
LorcanJConnolly <lconnolly.lorcan@gmail.com>
2025-06-10 21:12:32 +0100

    initialisation of dedicated domain object for account history

---
+++ src/main/java/com/example/domain/model/accountmanager/AccountHistory.java
@@ -0,0 +1,23 @@
+package com.example.domain.model.accountmanager;
+
+import com.example.domain.model.journal.Journal;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.HashMap;
+
+public class AccountHistory {
+    // TODO: fix type
+    private final HashMap<LocalDateTime, HashMap<Integer, ArrayList<Journal>>> tree;
+
+    public AccountHistory(LocalDateTime accountInitialisation, double initialValue){
+        this.tree = new HashMap<>();
+        storeEvent(accountInitialisation, initialValue, null);
+    }
+
+    public void storeEvent(LocalDateTime date, double valueAtDate, Journal journal){
+        if (journal == null && !this.tree.isEmpty()){
+            throw new IllegalArgumentException("Journal cannot be null.");
+        }
+    }
+}
