6a40c5bdd7d6409e2eb390862deebb5e8ba39e16
LorcanJConnolly <lconnolly.lorcan@gmail.com>
2025-06-13 09:44:46 +0100

    first implementation of basic methods

--- src/main/java/com/example/domain/model/accountmanager/AccountHistory.java
+++ src/main/java/com/example/domain/model/accountmanager/AccountHistory.java
@@ -1,23 +1,51 @@
 package com.example.domain.model.accountmanager;
 
+import com.example.AccountCategory;
 import com.example.domain.model.journal.Journal;
 
 import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
+import java.util.TreeMap;
 
 public class AccountHistory {
     // TODO: fix type
-    private final HashMap<LocalDateTime, HashMap<Integer, ArrayList<Journal>>> tree;
+    private final TreeMap<LocalDateTime, AccountHistoryEntry> tree;
+
+    private class AccountHistoryEntry{
+        private double value;
+        private List<Journal> journals;
 
 
-    public AccountHistory(){
-        this.tree = new HashMap<>();
+        public AccountHistoryEntry(double value){
+            this.value = value;
+            this.journals = new ArrayList<>();
+        }
+
+        public void addJournal(Journal journal){
+            this.value += journal.getVa
+        }
     }
 
-    public void storeEvent(LocalDateTime date, double valueAtDate, Journal journal){
+    public AccountHistory(){
+        this.tree = new TreeMap<>();
+    }
+
+    public void storeJournal(LocalDateTime date, double valueAtDate, Journal journal){
         if (journal == null && !this.tree.isEmpty()){
             throw new IllegalArgumentException("Journal cannot be null.");
         }
+
+        LocalDateTime journalDate = journal.getDate();
+
+        if (this.tree.containsKey(journalDate)){
+            HashMap<Double, ArrayList<Journal>> entryAtDate = this.tree.get(journalDate);
+            entryAtDate.put(valueAtDate)
+        }
+    }
+
+    public void removeJournal(Journal journal){
+
     }
 }
