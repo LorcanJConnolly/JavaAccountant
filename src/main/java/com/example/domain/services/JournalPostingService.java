b4caf10b92e2124fbe2fc929950451f23b576704
LorcanJConnolly <lconnolly.lorcan@gmail.com>
2025-06-16 08:20:32 +0100

    TODOs

--- src/main/java/com/example/domain/services/JournalPostingService.java
+++ src/main/java/com/example/domain/services/JournalPostingService.java
@@ -4,23 +4,23 @@ import com.example.domain.model.accountmanager.Account;
 import com.example.domain.model.accountmanager.AccountsManager;
 import com.example.domain.model.journal.Journal;
 
-import java.util.UUID;
+import java.util.HashSet;
 
+// TODO Jounral does not go to accounts, its adjustments do > we just give the journal to the account manager and unpack it
 public class JournalPostingService {
-    public void PostJournalToAccounts(AccountsManager accountsManager, String stringAccountId, Journal journal) {
+    public void PostJournalToAccounts(AccountsManager accountsManager, HashSet<String> accountIdentifiers, Journal journal) {
         if (journal.getTotal() != 0) {
             throw new IllegalArgumentException("Cannot post unbalanced Journal. Please balance adjustments.");
         }
-        UUID accountId = UUID.fromString(stringAccountId);
-        Account account = accountsManager.findAccountById(accountId);
+
+        Account account = accountsManager.findAccountByIdentifiers(accountIdentifiers);
         for (Journal.Adjustment adjustment : journal.getAdjustments()) {
             account.applyAdjustment(journal.getDate(), adjustment);
         }
     }
 
-    public void deleteJournalFromAccounts(AccountsManager accountsManager, String stringAccountId, Journal journal) {
-        UUID accountId = UUID.fromString(stringAccountId);
-        Account account = accountsManager.findAccountById(accountId);
+    public void deleteJournalFromAccounts(AccountsManager accountsManager, HashSet<String> accountIdentifiers, Journal journal) {
+        Account account = accountsManager.findAccountByIdentifiers(accountIdentifiers);
         for (Journal.Adjustment adjustment : journal.getAdjustments()) {
             account.removeAdjustment(journal.getDate(), adjustment);
         }
