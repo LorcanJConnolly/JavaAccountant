8ad43ec92ec2fbf6941f03ab7b06f4b8f9dde430
LorcanJConnolly <lconnolly.lorcan@gmail.com>
2025-06-16 08:19:24 +0100

    added fromString init

--- src/main/java/com/example/domain/model/accountmanager/AccountCategory.java
+++ src/main/java/com/example/domain/model/accountmanager/AccountCategory.java
@@ -1,5 +1,13 @@
 package com.example.domain.model.accountmanager;
 
 public enum AccountCategory {
-    ASSET, LIABILITY , EQUITY, REVENUE, EXPENSE
+    ASSET, LIABILITY , EQUITY, REVENUE, EXPENSE;
+
+    public static AccountCategory fromString(String input) {
+        try {
+            return AccountCategory.valueOf(input);
+        } catch (IllegalArgumentException | NullPointerException e) {
+            throw new IllegalArgumentException("Category '" + input + "' is not a member of the AccountCategory enum.");
+        }
+    }
 }
